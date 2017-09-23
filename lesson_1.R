689fb557621af2461b48ca1c49e0354c645e484b
zankay_balazs <zankay_balazs@student.ceu.edu>
2017-09-23 15:29:26 +0200

    First Lesson notes

---
+++ lesson_1.R
@@ -0,0 +1,77 @@
+1 + 3
+2*3
+3^2
+str(pi)
+str('pi')
+letters
+str(letters)
+LETTERS
+LETTERS
+letters [10]
+LETTERS[1]
+letters[c(1,10)]
+?str.dendrogram?str.dendrogram
+x <- 2
+x*2
+# todo square root of x
+x^0.5
+sqrt (x)
+#♣
+# random numbers
+runif(1)
+set.seed(42)
+runif(1)
+x <- 4
+runif(5)
+round(runif(5))
+round(runif(5,digit=2))
+round(runif(5, digits=2))
+round(digits=2, x= runif(5))
+# todo gen 1000 random number between 0 and 10
+# digits = 2 => visualize
+round(digits=2, x= runif (1000))
+ggplot2: x
+plot x11()
+x <- runif(1000, min=0, max=10)
+x <- runif (1000) * 10
+x <- round(x,2)
+x <-
+  plot (x)
+hist (x)
+f <- function(x) 2 * x + 1
+f(5)
+f(pi)
+f(1:5)
+1:
+  1:5
+f <- function (x) { X vectorized}
+x <- 1:5
+f˙(x)
+f (x)
+plot (x, f(x))
+plot (x, f(x)), type= 'l'
+x <- seq(1, 5*pi, by=0.1)
+plot (x, f(x)), type = l
+x <- seq(1 ,5, by = 0.1)
+plot(x, sin(x), type = 'l')
+# random walk example
+round(runif(25)) *2 -1
+sum (x)
+cumsum (x)
+plot (cumsum (x), type= 's')
+h <- c(174, 170, 160)
+w <- c(90, 80, 70)
+plot(h, w, xlab = 'height', ylab = 'weight'
+     cor(h, w)
+     plot(h, w, xlab = 'height', ylab = 'weight')
+     cor(h, w)
+     lm(w~w)
+     lm(w ~ h) # formula notation
+     summary (fit)
+     fit <- lm(w~h) # formula notation
+     summary (fit)
+     abline (fit)
+     abline (fit, col = 'red')
+     predict (fit, list(h = 174))
+     predict = (fit, list(h =185))
+     predict (fit, list (h = 185))
